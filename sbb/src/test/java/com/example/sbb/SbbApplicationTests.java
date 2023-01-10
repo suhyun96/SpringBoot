67292da157d2c4e15e27c00f38eef8eb8a45b712
suhyun96 <jhk8500@gmail.com>
2023-01-10 23:33:02 +0900

    회원 서비스 개발
    2023/01/10

--- sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -38,14 +38,14 @@ class SbbApplicationTests {
     @Test
     void testJpa() {
         Question q1 = new Question();
-        q1.setSubject("sbb란? ");
+        q1.setSubject("sbb란?");
         q1.setContent("sbb 개념과 구조에 대해서 알고 싶습니다");
         q1.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q1);
 
         Question q2 = new Question();
-        q2.setSubject("스프링 부트란? ");
-        q2.setContent("id는 자동 생성되나요? ");
+        q2.setSubject("스프링 부트란?");
+        q2.setContent("id는 자동 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);
 
